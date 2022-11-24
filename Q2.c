1ebd347849d6b8672056a87570fe2d3a0e7546a7
rajorshi1 <108346962+rajorshi1@users.noreply.github.com>
2022-11-24 14:08:23 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -43,65 +43,27 @@ int main() {
         execl("/bin/sh","sh","sakura.sh", (char *) NULL);
         exit(0);
     }
-    pid_t res = wait(NULL);
-    clock_gettime(CLOCK_MONOTONIC,&after1);
-    if (res == c1) {
-        unsigned long time1 = (before1.tv_sec * 1000000000) + before1.tv_nsec;
-        unsigned long time2 = (after1.tv_sec * 1000000000) + after1.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[0] = time_taken;
-    }
-    else if (res == c2)  {
-        unsigned long time1 = (before2.tv_sec * 1000000000) + before2.tv_nsec;
-        unsigned long time2 = (after1.tv_sec * 1000000000) + after1.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[1] = time_taken;
-    }
-    else if (res == c3) {
-        unsigned long time1 = (before3.tv_sec * 1000000000) + before3.tv_nsec;
-        unsigned long time2 = (after1.tv_sec * 1000000000) + after1.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[2] = time_taken;
-    }
-    res = wait(NULL);
-    clock_gettime(CLOCK_MONOTONIC,&after2);
-    if (res == c1) {
-        unsigned long time1 = (before1.tv_sec * 1000000000) + before1.tv_nsec;
-        unsigned long time2 = (after2.tv_sec * 1000000000) + after2.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[0] = time_taken;
-    }
-    else if (res == c2)  {
-        unsigned long time1 = (before2.tv_sec * 1000000000) + before2.tv_nsec;
-        unsigned long time2 = (after2.tv_sec * 1000000000) + after2.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[1] = time_taken;
-    }
-    else if (res == c3) {
-        unsigned long time1 = (before3.tv_sec * 1000000000) + before3.tv_nsec;
-        unsigned long time2 = (after2.tv_sec * 1000000000) + after2.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[2] = time_taken;
-    }
-    res = wait(NULL);
-    clock_gettime(CLOCK_MONOTONIC,&after3);
-    if (res == c1) {
-        unsigned long time1 = (before1.tv_sec * 1000000000) + before1.tv_nsec;
-        unsigned long time2 = (after3.tv_sec * 1000000000) + after3.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[0] = time_taken;
-    }
-    else if (res == c2)  {
-        unsigned long time1 = (before2.tv_sec * 1000000000) + before2.tv_nsec;
-        unsigned long time2 = (after3.tv_sec * 1000000000) + after3.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[1] = time_taken;
-    }
-    else if (res == c3) {
-        unsigned long time1 = (before3.tv_sec * 1000000000) + before3.tv_nsec;
-        unsigned long time2 = (after3.tv_sec * 1000000000) + after3.tv_nsec;
-        float time_taken = ((float)(time2 - time1))/1000000000;
-        times[2] = time_taken;
+    for (int i=0; i<3; i++) {
+        pid_t res = wait(NULL);
+        clock_gettime(CLOCK_MONOTONIC,&after1);
+        if (res == c1) {
+            unsigned long time1 = (before1.tv_sec * 1000000000) + before1.tv_nsec;
+            unsigned long time2 = (after1.tv_sec * 1000000000) + after1.tv_nsec;
+            float time_taken = ((float)(time2 - time1))/1000000000;
+            times[0] = time_taken;
+        }
+        else if (res == c2)  {
+            unsigned long time1 = (before2.tv_sec * 1000000000) + before2.tv_nsec;
+            unsigned long time2 = (after1.tv_sec * 1000000000) + after1.tv_nsec;
+            float time_taken = ((float)(time2 - time1))/1000000000;
+            times[1] = time_taken;
+        }
+        else if (res == c3) {
+            unsigned long time1 = (before3.tv_sec * 1000000000) + before3.tv_nsec;
+            unsigned long time2 = (after1.tv_sec * 1000000000) + after1.tv_nsec;
+            float time_taken = ((float)(time2 - time1))/1000000000;
+            times[2] = time_taken;
+        }
     }
     printf("Time Taken with priority 1: %f\n", times[0]);
     printf("Time Taken with priority 20: %f\n", times[1]);
